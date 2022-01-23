5e42000f6bc58b146a0972f5fa786ec5f0683256
AnjanaLP <anjana.lpn@gmail.com>
2022-01-23 16:55:36 +0000

    setup project infrastructure

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+ruby '3.1.0'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'webrick'
+
+group :test do
+  gem 'capybara'
+  gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+])
+SimpleCov.start
+
+Capybara.app = Chitter
