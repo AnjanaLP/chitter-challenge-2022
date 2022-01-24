1e25a4b60f1d5179c2ec1c9bc8a7b7fec978b69d
AnjanaLP <anjana.lpn@gmail.com>
2022-01-24 00:02:57 +0000

    extract display of time and date into a view helper method

--- app/helpers.rb
+++ app/helpers.rb
@@ -1,5 +1,9 @@
 module Helpers
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) 
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def time_and_date(time)
+    time.strftime('at %H:%M on %d-%m-%Y')
   end
 end
